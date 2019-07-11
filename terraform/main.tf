55778ebc137d8ba25dfac3a3f1241cf74c524546
Felddy <markf@geekpad.com>
2019-07-11 17:12:47 -0400

    Add new SSM param requirements

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ module "iam_user" {
   source = "github.com/cisagov/molecule-packer-travisci-iam-user-tf-module"
 
   add_packer_permissions = true
-  ssm_parameters         = ["/cyhy/dev/users", "/ssh/public_keys/*"]
+  ssm_parameters         = ["/cyhy/dev/users", "/ssh/public_keys/*", "/openvpn/server/*"]
   user_name              = "test-openvpn-packer"
   tags = {
     Team        = "CISA - Development"
