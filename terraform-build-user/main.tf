e70b591b680758956dae65d8af13fd972d8953cf
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-10-04 15:59:31 -0400

    Revert "Update TF code for build user"
    This _partially_ reverts commit
    c1c62c4dfb1bd2233ea9be89101966182b4ea5b9, adding back in the necessary
    support for the Nessus and Tanium CDM agents.

--- terraform-build-user/main.tf
+++ terraform-build-user/main.tf
@@ -10,11 +10,72 @@ module "iam_user" {
   }
 
   ssm_parameters = [
+    "/cdm/tanium_hostname",
     "/cyhy/dev/users",
     "/openvpn/server/*",
     "/ssh/public_keys/*",
   ]
   user_name = "build-openvpn-packer"
+}
+
+# Attach 3rd party S3 bucket read-only policy from
+# cisagov/ansible-role-cdm-tanium-client to the production
+# EC2AMICreate role
+resource "aws_iam_role_policy_attachment" "thirdpartybucketread_tanium_production" {
+  provider = aws.images-production-ami
+
+  policy_arn = data.terraform_remote_state.ansible_role_cdm_tanium_client.outputs.production_bucket_policy.arn
+  role       = module.iam_user.ec2amicreate_role_production.name
+}
+
+# Attach 3rd party S3 bucket read-only policy from
+# cisagov/ansible-role-cdm-tanium-client to the staging EC2AMICreate
+# role
+resource "aws_iam_role_policy_attachment" "thirdpartybucketread_tanium_staging" {
+  provider = aws.images-staging-ami
+
+  policy_arn = data.terraform_remote_state.ansible_role_cdm_tanium_client.outputs.staging_bucket_policy.arn
+  role       = module.iam_user.ec2amicreate_role_staging.name
+}
+
+# Attach 3rd party S3 bucket read-only policy from
+# cisagov/ansible-role-cdm-nessus-agent to the production
+# EC2AMICreate role
+resource "aws_iam_role_policy_attachment" "thirdpartybucketread_nessus_production" {
+  provider = aws.images-production-ami
+
+  policy_arn = data.terraform_remote_state.ansible_role_cdm_nessus_agent.outputs.production_bucket_policy.arn
+  role       = module.iam_user.ec2amicreate_role_production.name
+}
+
+# Attach 3rd party S3 bucket read-only policy from
+# cisagov/ansible-role-cdm-nessus-agent to the staging EC2AMICreate
+# role
+resource "aws_iam_role_policy_attachment" "thirdpartybucketread_nessus_staging" {
+  provider = aws.images-staging-ami
+
+  policy_arn = data.terraform_remote_state.ansible_role_cdm_nessus_agent.outputs.staging_bucket_policy.arn
+  role       = module.iam_user.ec2amicreate_role_staging.name
+}
+
+# Attach 3rd party S3 bucket read-only policy from
+# cisagov/ansible-role-cdm-certificates to the production EC2AMICreate
+# role
+resource "aws_iam_role_policy_attachment" "thirdpartybucketread_certificates_production" {
+  provider = aws.images-production-ami
+
+  policy_arn = data.terraform_remote_state.ansible_role_cdm_certificates.outputs.production_bucket_policy.arn
+  role       = module.iam_user.ec2amicreate_role_production.name
+}
+
+# Attach 3rd party S3 bucket read-only policy from
+# cisagov/ansible-role-cdm-certificates to the staging EC2AMICreate
+# role
+resource "aws_iam_role_policy_attachment" "thirdpartybucketread_certificates_staging" {
+  provider = aws.images-staging-ami
+
+  policy_arn = data.terraform_remote_state.ansible_role_cdm_certificates.outputs.staging_bucket_policy.arn
+  role       = module.iam_user.ec2amicreate_role_staging.name
 }
 
 # Attach 3rd party S3 bucket read-only policy from
